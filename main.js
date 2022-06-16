138db09315224b2d48c1abe76c0dc89d788061f7
Aizada <ybykeeevaaizada@gmail.com>
2022-06-16 12:40:48 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let childNum = 2;
+let partName = "Nurlan";
+let loc = "Bishkek";
+let job = "cool developer";
+console.log ('You will be a ' +job+' in '+loc+ ', and married to ' +partName+ ' with ' +childNum+ ' kids.')
